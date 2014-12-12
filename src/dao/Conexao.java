810d1d9d0af4b74a7de1d43078c4ef007bee6739
01aluno <01aluno@01DAIL2727.ifma.edu>
2014-12-12 15:29:22 -0300

    Classe de conexão adicionada.

---
+++ src/dao/Conexao.java
@@ -0,0 +1,34 @@
+package dao;
+
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Connection;
+
+public class Conexao {
+	private Connection conexao;
+	private String url = "jdbc:mysql//localhost/";
+	private String banco = "bdgameranking";
+	private String usuario = "root";
+	private String senha = "";
+
+	public Conexao() {
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			conexao = DriverManager.getConnection(url, usuario, senha);
+		} catch (ClassNotFoundException e) {
+			System.out.println("Class not found in System, =)");
+			e.printStackTrace();
+		} catch (SQLException e) {
+			System.out.println("BD not found in System, =)");
+			e.printStackTrace();
+		}
+
+	}
+
+	private Connection retornaConexao() {
+
+		return this.conexao;
+
+	}
+
+}
